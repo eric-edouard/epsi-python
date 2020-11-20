068817f449c8c8d589133d9dbea2940f5b12df02
Eric <eric.amilhat@epitech.eu>
2020-11-20 09:31:57 +0100

    added modules

--- course/10_modules.py
+++ course/10_modules.py
@@ -1,2 +1,13 @@
 # A module is basically a file containing a set of functions to include in your application. 
 # There are core python modules, modules you can install using the pip package manager as well as custom modules
+
+# import datetime
+from datetime import date
+
+from camelcase import CamelCase
+
+today = date.today()
+print(today)
+
+c = CamelCase()
+print(c.hump("this is a camel case string"))
